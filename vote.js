1eb89b29cc6e9174e158f9509c345a7203534165
kattakadachand <kattakadachandran16@gmail.com>
2026-03-14 10:22:53 +0530

    HELLO WORLD

---
+++ vote.js
@@ -0,0 +1,12 @@
+function vote() {
+    var name = document.getElementById("name").value;
+    var age = document.getElementById("age").value;
+    var res = document.getElementById("res");
+
+    if (age >= 18) {
+        alert("Eligible to vote");
+        res.innerHTML = name + " eligible to vote";
+    } else {
+        res.innerHTML = name + " you are still a child";
+    }
+}
